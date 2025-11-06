48888fa121e1f94eb5a680d5cc87f1c2c2388385
Anshika <asatianshika14@gmail.com>
2025-11-06 17:05:47 +0530

    cleanup according to js

--- backend/build/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.deepClone = deepClone;
-function deepClone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-}
